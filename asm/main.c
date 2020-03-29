d1b84a262f30a73c040d3e78ce8f11faf1c8272f
Minnerlas <nikilirikili@gmail.com>
2020-03-29 23:41:54 +0200

    Manje izmene

--- asm/main.c
+++ asm/main.c
@@ -2,40 +2,94 @@
 #include <string.h>
 #include <stdlib.h>
 #include "../vm/vm.h"
+#include "hmapa.h"
+
+typedef struct {
+
+} asembler;
+
 
 int main(int argc, const char* argv[]){
 
-	char ulaz[20]={0};
+	char ulaz[50]={0};
+	char izlaz[50]={0};
 	int stdulaz=1;
+	int stdizlaz=1;
+	int kraj=0;
 
 	for(int i=1; i<argc; i++){
+		//printf("%s ", argv[i]);
 		if(argv[i][0]=='-')
 			switch(argv[i][1]){
 				case 'o':
+					stdizlaz=0;
+					if(i<argc)
+						strcpy(izlaz, argv[++i]);
+					else{
+						printf("Upotreba: -o <izlazni fajl>\n");
+						exit(1);
+					}
+					break;
 				default:
-					printf("Nepoznata komanda: %s.\n",argv[i]);
+					printf("Nepoznata komanda: %s.\n", argv[i]);
 					exit(1);
 			}
 		else{
 			stdulaz=0;
-			strcpy(ulaz,argv[i]);
+			strcpy(ulaz, argv[i]);
 		}
 	}
+	//putchar('\n');
 
-	registar prog[NRAM]={0};
-	prog[NRAM-1]=HLT;
+	char t[100];
 
-	if(!stdulaz){
-		registar *t=prog;
-		int n=0;
-		FILE *fp=fopen(ulaz, "r");
-		while((fscanf(fp, "%x|", t++)!=EOF)&&(n++<NRAM));
-		fclose(fp);
-	}else{
-		registar *t=prog;
-		int n=0;
-		while((scanf("%x|",t++)!=EOF)&&(n++<NRAM));
-	}
+
+
+	FILE *ul, *iz;
+
+	if(stdulaz)
+		ul=stdin;
+	else
+		ul=fopen(ulaz, "r");
+
+	
+	if(stdizlaz)
+		iz=stdout;
+	else
+		iz=fopen(izlaz, "w");
+	
+
+	do {
+		switch(c=fgetc(ul)){
+			case EOF:
+				kraj=1;
+				break;
+
+			case '\t':
+				break;
+
+			default:
+
+		}
+
+	} while(!kraj);
+
+
+	fscanf(ul, "%s", &t);
+	fprintf(iz, "%s", t);
+
+	printf("%s\n",t);
+
+
+
+
+
+
+	if(!stdizlaz)
+		fclose(iz);
+
+	if(!stdulaz)
+		fclose(ul);
 
 	return 0;
 }
